7211c34ee28d5ee7cdc9b85c6f497259c2d5b65c
saiem-jatri <shahedsaiem.jatri@gmail.com>
2022-08-17 17:18:55 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,95 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      searchString: '',
+        inputTask:'',
+        showMessege:false,
+        nextId:14,
+        currentPage:0,
+        pageSize:3,
+        visibleTodos:[],
+        sortBy:'name',
+        
+        todos: [
+          {id:1,name:"shahed",isComplete:false},
+          {id:2,name:"saiem",isComplete:false},
+          {id:3,name:"mohaimen",isComplete:true},
+          {id:4,name:"mohaimen",isComplete:false},
+          {id:5,name:"mohaimen",isComplete:true},
+          {id:6,name:"mohaimen",isComplete:false},
+          {id:7,name:"mohaimen",isComplete:true},
+          {id:8,name:"mohaimen",isComplete:true},
+          {id:9,name:"mohaimen",isComplete:false},
+          {id:10,name:"mohaimen",isComplete:true},
+          {id:11,name:"mohaimen",isComplete:false},
+          {id:12,name:"mohaimen",isComplete:true},
+          {id:13,name:"mohaimen",isComplete:false},
+        ],
+     };
+  },
+  created() {
+    this.updateVisibleTodos();
+  },
+  methods: {
+    addTask() {
+      if(this.inputTask == ''){
+        setTimeout(()=>{
+          this.showMessege = false
+        },2000)
+        this.showMessege = true
+        return
+      }
+      this.todos.unshift({name:this.inputTask, isComplete:false})
+      this.inputTask=''
+      this.updateVisibleTodos();
+  },
+  
+  deleteTask(index) {
+    this.todos.splice(index, 1);
+    this.updateVisibleTodos();
+},
+
+changeStatus(){
+ this.filterItems.isComplete = !this.filterItems.isComplete
+},
+    
+updatePage(pageNumber){
+  this.currentPage = pageNumber;
+  this.updateVisibleTodos();
+},
+
+updateVisibleTodos(){
+   this.visibleTodos = this.todos.slice(this.currentPage * this.pageSize, (this.currentPage * this.pageSize) + this.pageSize);
+    // if there are no visible todos, go back page
+    if(this.visibleTodos.length == 0 && this.currentPage > 0){
+      this.updatePage(this.currentPage - 1);
+    }
+  },
+},
+
+  computed: {
+    filterItems(){
+      return this.visibleTodos.filter(items=> items.name.toLowerCase().includes(this.searchString))
+    },
+
+    afterFilter(){
+      return this.visibleTodos.filter(items =>{
+        return items.isComplete === true;
+      })
+    },
+    totalPages(){
+     return Math.ceil(this.todos.length / this.pageSize)
+    },
+    showPreviousLink(){
+      return this.currentPage == 0 ? false : true;
+    },
+
+    showNextLink(){
+      return this.currentPage == (this.totalPages -1) ? false : true;
+    }, 
+
+}
+
+});
+
+app.mount('#user-goals');
